8a9732c4191429806f7be95ef8eaaa43381c923b
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2017-06-21 22:17:06 +0200

    Add Docker hub deployment step to CircleCI build

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,4 +2,4 @@
 dotnet publish ./src/SlowSlothBudget.ExpensesService -c Release -o out
 cp -r ./src/SlowSlothBudget.ExpensesService/out ./out
 docker info
-docker build -t gcichosz/expenses-service .
+docker build -t gcichosz/slowslothbudget-expensesservice .
